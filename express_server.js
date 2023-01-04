0ea0bf37f38c52aea855d41eb027d4fe4caa8835
David Figueroa <figueroadavid93@gmail.com>
2023-01-04 14:02:54 -0500

    random ID generator function implemented and post req for /urls

--- express_server.js
+++ express_server.js
@@ -1,35 +1,69 @@
+////////////////////////////////////////EXPRESS/////////////////////////////////////////
 const express = require("express");
 const app = express();
 const PORT = 8080;
 
 
+////////////////////////////////////////MIDDLEWARE//////////////////////////////////////
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true}));
 
 
+/////////////////////////////////////////FUNCTIONS////////////////////////////////////////
+function generateRandomString() {
+  let result = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  charactersLength = characters.length;
+
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
+
+
+//////////////////////////////////////////DATABASE//////////////////////////////////////
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 
+/////////////////////////////////////ROUTES/////////////////////////////////////////////
 
+//Random Practice Routes//
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
+//URLS HOMEPAGE//
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+//CREATE NEW URL PAGE//
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+//SINGLE URL PAGE//
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id
   const longURL = urlDatabase[id]
